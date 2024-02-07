d128583e3c4a23eb901cdc85fcec8b71dc1fb880
Gomohit <99570471+Gomohit@users.noreply.github.com>
2024-02-07 12:12:49 +0000

    add host

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0##8gmuc6@5xm563#&up530o_9wj#3z@o713w4@zlt4&@7qa=%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
